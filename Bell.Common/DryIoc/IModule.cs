7f02f14fb0848562fcc2cca8dceb9d6752f8591b
Brandon Bell <fizzleban@yahoo.com>
2017-01-09 22:20:05 +0000

    Setting up project

---
+++ Bell.Common/DryIoc/IModule.cs
@@ -0,0 +1,14 @@
+﻿using DryIoc;
+
+namespace Bell.Common.DryIoc
+{
+    public interface IModule
+    {
+        /// <summary> 
+        /// Called when the module is loaded
+        /// </summary>
+        /// <param name="builder">The registration role of the DryIoc container</param>
+        /// <remarks>Used to register the module's types</remarks>
+        void Load(IRegistrator builder);
+    }
+}
---
+++ Bell.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bell.Common.WebApi.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public RedirectResult Index()
+        {
+            return Redirect("api-explorer");
+        }
+    }
+}
