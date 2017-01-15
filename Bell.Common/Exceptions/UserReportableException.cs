9d690b2ef7c9a26d35c3da24c626d364fa443420
Brandon Bell <fizzleban@yahoo.com>
2017-01-15 23:40:03 +0000

    Updating logging and exceptions

---
+++ Bell.Common/Exceptions/UserReportableException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bell.Common.Exceptions
+{
+    /// <summary>
+    ///  Represents a user reportable exception
+    /// </summary>
+    public class UserReportableException: Exception
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Instantiates a user reportable exception
+        /// </summary>
+        /// <param name="errorCode">The error code for the exception</param>
+        public UserReportableException(string errorCode): base()
+        {
+            ErrorCode = errorCode;
+        }
+
+        /// <summary>
+        /// Instantiates a user reportable exception
+        /// </summary>
+        /// <param name="errorCode">The error code for the exception</param>
+        /// <param name="innerException">The inner exception</param>
+        public UserReportableException(string errorCode, Exception innerException): base(errorCode, innerException)
+        {
+            ErrorCode = errorCode;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the error code of the exception
+        /// </summary>
+        public string ErrorCode { get; private set; }
+
+        #endregion
+    }
+}
---
+++ Bell.WebApi/Exceptions/ErrorHandlingMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Bell.Common.Exceptions
+{
+    public class ErrorHandlingMiddleware
+    {
+        #region Private Fields
+
+        private readonly RequestDelegate next;
+
+        #endregion
+
+        #region Constructors
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            if (exception == null) return;
+
+            var code = HttpStatusCode.InternalServerError; 
+
+            //if (exception is MyNotFoundException) code = HttpStatusCode.NotFound;
+            //else if (exception is MyUnauthorizedException) code = HttpStatusCode.Unauthorized;
+            //else if (exception is MyException) code = HttpStatusCode.BadRequest;
+
+            await WriteExceptionAsync(context, code, exception).ConfigureAwait(false);
+        }
+
+        private static async Task WriteExceptionAsync(HttpContext context, HttpStatusCode code, Exception exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int)code;
+            await response.WriteAsync(JsonConvert.SerializeObject(new
+            {
+                error = new
+                {
+                    message = exception.Message,
+                    exception = exception.GetType().Name
+                }
+            })).ConfigureAwait(false);
+        }
+
+        #endregion
+    }
+}
