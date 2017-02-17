8fdb52b45bf96bb20c7d2956609032dc0816a3dd
Brandon Bell <fizzleban@yahoo.com>
2017-02-17 17:30:27 +0000

    Adding UserLoginService

---
+++ Bell.Common/Services/UserLoginService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bell.Common.Models;
+using Bell.Common.Services;
+
+namespace Bell.Common.Services
+{
+    public interface IUserLoginService
+    { 
+        UserLoginResponse Login(string username, string password);
+    }
+
+    public class UserLoginService : IUserLoginService
+    {
+    }
+}
